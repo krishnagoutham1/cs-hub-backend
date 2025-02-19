3837727800497aadd71a76591eae19da8504c528
krishnaj91 <krishnagoutham91@gmail.com>
2025-02-19 12:15:53 +0530

    mysql db setup/connection

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Sequelize
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
+async function connectDB() {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ MySQL Connected...");
+  } catch (error) {
+    console.error("❌ Error connecting to MySQL:", error);
+    process.exit(1);
+  }
+}
+
+module.exports = { sequelize, connectDB };
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { connectDB } = require("./config/db");
 
 dotenv.config();
+
 const PORT = process.env.PORT;
 
 const app = express();
+
+// Mysql connection
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("hello world");
